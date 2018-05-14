005926def5692a79baa907a562f1a3280791b7a6
Purvesh <ind.purvesh@gmail.com>
2018-05-15 09:33:37 +1200

    updated composer json

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,7 @@
   | and give it the controller to call when that URI is requested.
   |
  */
-
+use Illuminate\Support\Facades\Route;
 
 Route::middleware(['web'])
     ->namespace('AvoRed\Contact\Http\Controllers')
